b9b2a36f75c92ef8ccee4bb007f0c2cd16f931bd
shmatkoden <smatkoden@gmail.com>
2024-09-04 23:37:58 +0300

    Initial commit: added basic 'Hello, World!' site

---
+++ app.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Устанавливаем путь к папке с файлами HTML
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
