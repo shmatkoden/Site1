a2fa365e386f9b0390a8f6982f43cddd6c972d6b
shmatkoden <smatkoden@gmail.com>
2024-09-05 00:12:50 +0300

    2

--- app.js
+++ app.js
@@ -1,13 +1,23 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const paymentRoutes = require('./routes/paymentRoutes');
+
 const app = express();
 
-// Устанавливаем путь к папке с файлами HTML
-app.use(express.static(path.join(__dirname, 'public')));
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(paymentRoutes);
+
+// Маршрут для корневого URL, который теперь показывает payment.ejs
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'public', 'index.html'));
+    res.render('payment');
 });
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(333, () => {
+    console.log('Server is running on port 3000');
+});
